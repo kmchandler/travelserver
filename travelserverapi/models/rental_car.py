92e1b1220e564280f30a52516a0b5fb0ffb19202
Kristen Chandler <krismchandler@gmail.com>
2023-07-03 15:04:59 -0500

    updated related names in models

--- travelserverapi/models/rental_car.py
+++ travelserverapi/models/rental_car.py
@@ -3,7 +3,7 @@ from . import ItineraryItem
 from . import Address
 
 class RentalCar(ItineraryItem):
-    itinerary_item = models.ForeignKey(ItineraryItem, related_name="rental cars", on_delete=models.CASCADE)
+    itinerary_item = models.ForeignKey(ItineraryItem, related_name="rentalcars", on_delete=models.CASCADE)
     address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
     phone_number = models.CharField(max_length=100)
     pickup_time = models.TimeField()
--- travelserverapi/models/train.py
+++ travelserverapi/models/train.py
@@ -5,8 +5,8 @@ from . import Address
 class Train(ItineraryItem):
     itinerary_item = models.ForeignKey(ItineraryItem, related_name="trains", on_delete=models.CASCADE)
     departure_station = models.CharField(max_length=100)
-    departure_station_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+    departure_station_address = models.ForeignKey(Address, related_name="departure", on_delete=models.SET_NULL, null=True)
     departure_time = models.TimeField()
     arrival_station = models.CharField(max_length=100)
-    arrival_station_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+    arrival_station_address = models.ForeignKey(Address, related_name="arrival", on_delete=models.SET_NULL, null=True)
     arrival_time = models.TimeField()
